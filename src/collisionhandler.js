3321678f92eb74a1c8d5c20a2b3fa886ed1fbe64
Trinhhuutung100 <trinhhuutung100@gmail.com>
2023-06-22 17:07:39 +0700

    Revert "remove some excess code"
    This reverts commit cd9f72ba90126dd4d12b5e9d1617d950ce4b2781.

--- src/collisionhandler.js
+++ src/collisionhandler.js
@@ -13,38 +13,40 @@ export class CollisionHandler{
     }
     squareCollision(){
         for(var s = 0; s< this.squares.length; s++){
-            for(var b = 0; b< this.balls.length; b++){
-                if(this.squares[s].index == 0) {
-                    this.squares[s].destroy();
-                    continue;
-                }
-                var ballThanSquareX = this.balls[b].ball.x > this.squares[s].square.x?true:false;
-                var ballThanSquareY = this.balls[b].ball.y > this.squares[s].square.y?true:false;
+            for(var s = 0; s< this.squares.length; s++){
+                for(var b = 0; b< this.balls.length; b++){
+                    if(this.squares[s].index == 0) {
+                        this.squares[s].destroy();
+                        continue;
+                    }
+                    var ballThanSquareX = this.balls[b].ball.x > this.squares[s].square.x?true:false;
+                    var ballThanSquareY = this.balls[b].ball.y > this.squares[s].square.y?true:false;
 
-                var ballX = this.balls[b].ball.x;
-                var ballY = this.balls[b].ball.y;
-                var squareX = this.squares[s].square.x;
-                var squareY = this.squares[s].square.y;         
+                    var ballX = this.balls[b].ball.x;
+                    var ballY = this.balls[b].ball.y;
+                    var squareX = this.squares[s].square.x;
+                    var squareY = this.squares[s].square.y;         
 
-                var distX = Math.abs(ballX + this.balls[b].dx - squareX);
-                var distY = Math.abs(ballY + this.balls[b].dy - squareY);
+                    var distX = Math.abs(ballX + this.balls[b].dx - squareX);
+                    var distY = Math.abs(ballY + this.balls[b].dy - squareY);
 
-                if(distX<=edge){
-                    if(distY<edge+ballRadius){
+                    if(distX<=edge){
+                        if(distY<edge+ballRadius){
+                            this.balls[b].dy = - this.balls[b].dy;
+                            continue;
+                        }
+                    } 
+                    if(distY<=edge){
+                        if(distX<edge+ballRadius){
+                            this.balls[b].dx = - this.balls[b].dx;
+                            continue;
+                        }
+                    } 
+                    if(distX*distX + distY*distY<(ballRadius + edge)*(ballRadius + edge)){
                         this.balls[b].dy = - this.balls[b].dy;
-                        continue;
-                    }
-                } 
-                if(distY<=edge){
-                    if(distX<edge+ballRadius){
                         this.balls[b].dx = - this.balls[b].dx;
-                        continue;
-                    }
-                } 
-                if(distX*distX + distY*distY<(ballRadius + edge)*(ballRadius + edge)){
-                    this.balls[b].dy = - this.balls[b].dy;
-                    this.balls[b].dx = - this.balls[b].dx;
-                }                                                            
+                    }                                        
+                }
             }
         }
     }
