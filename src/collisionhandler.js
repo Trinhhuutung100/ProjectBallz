915de4dfbfc4d8aa9bcb6b240cac29eccc4162e9
Trinhhuutung100 <trinhhuutung100@gmail.com>
2023-06-22 17:08:15 +0700

    solve square collision

--- src/collisionhandler.js
+++ src/collisionhandler.js
@@ -13,41 +13,53 @@ export class CollisionHandler{
     }
     squareCollision(){
         for(var s = 0; s< this.squares.length; s++){
-            for(var s = 0; s< this.squares.length; s++){
-                for(var b = 0; b< this.balls.length; b++){
-                    if(this.squares[s].index == 0) {
-                        this.squares[s].destroy();
-                        continue;
+            for (var b = 0; b < this.balls.length; b++){
+                let ball = this.balls[b];
+                let square = this.squares[s].square;
+                if(this.squares[s].index == 0) {
+                    this.squares[s].destroy();
+                    continue;
+                }
+                if (this.isCollisionBetweenCircleAndSquare(ball.ball, square)) {
+                    if (ball.ball.x + ballRadius / 2> square.x + square.width / 2) {
+                        ball.dx = -ball.dx;
                     }
-                    var ballThanSquareX = this.balls[b].ball.x > this.squares[s].square.x?true:false;
-                    var ballThanSquareY = this.balls[b].ball.y > this.squares[s].square.y?true:false;
+                    if (ball.ball.x + ballRadius / 2< square.x - square.width / 2) { 
+                        ball.dx = -ball.dx;
+                    }
 
-                    var ballX = this.balls[b].ball.x;
-                    var ballY = this.balls[b].ball.y;
-                    var squareX = this.squares[s].square.x;
-                    var squareY = this.squares[s].square.y;         
-
-                    var distX = Math.abs(ballX + this.balls[b].dx - squareX);
-                    var distY = Math.abs(ballY + this.balls[b].dy - squareY);
-
-                    if(distX<=edge){
-                        if(distY<edge+ballRadius){
-                            this.balls[b].dy = - this.balls[b].dy;
-                            continue;
-                        }
-                    } 
-                    if(distY<=edge){
-                        if(distX<edge+ballRadius){
-                            this.balls[b].dx = - this.balls[b].dx;
-                            continue;
-                        }
-                    } 
-                    if(distX*distX + distY*distY<(ballRadius + edge)*(ballRadius + edge)){
-                        this.balls[b].dy = - this.balls[b].dy;
-                        this.balls[b].dx = - this.balls[b].dx;
-                    }                                        
+                    if (ball.ball.y + ballRadius / 2 > square.y + square.height / 2) { 
+                        ball.dy = -ball.dy;
+                    }
+                    if (ball.ball.y + ballRadius / 2 < square.y - square.height / 2) { 
+                        ball.dy = -ball.dy;
+                    }
                 }
             }
         }
+    }
+
+    isCollisionBetweenCircleAndSquare(circle, square) {
+        let distX = Math.abs(circle.x - square.x);
+        let distY = Math.abs(circle.y - square.y);
+        if (distX > square.width / 2 + ballRadius) {
+            return false;
+        }
+        if (distY > square.height / 2 + ballRadius) {
+            return false;
+        }
+
+        if (distX <= square.width / 2) {
+            return true;
+        }
+        if (distY <= square.height / 2) {
+            return true;
+        }
+
+        let dx = distX - square.width / 2;
+        let dy = distY - square.height / 2;
+
+        if (dx * dx + dy * dy <= ballRadius * ballRadius) return true;
+        return false;
     }
 }
