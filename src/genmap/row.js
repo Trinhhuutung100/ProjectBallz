deae17a4133972292d0bcb986d1bb21bb4f61232
HauMoon <conghau266255@gmail.com>
2023-07-20 22:29:49 +0700

    fix ok genmap, game dễ hơn

--- src/genmap/row.js
+++ src/genmap/row.js
@@ -39,28 +39,25 @@ export class Row {
                 var idxR=getRandomElementsFromArray(this.idx,1)[0];
                 this.data[idxR]=-2; //coin
             }
-            else{
-                this.data[idxR]=0;
-            }
         }
         var random2= Math.random();
         var idxX=[]; // mảng lưu các chỉ số của các ô lớn
         if(random2 <=0.75){
             idxX=getRandomElementsFromArray(this.idx,1);  //xác suất xuất hiện 1 ô lớn là 75%
         }
-        else if(random2<=0.87 && random2 >0.75){
+         if(random2<=0.87 && random2 >0.75){
              idxX=getRandomElementsFromArray(this.idx,2);// xác suất xuất hiện 2 ô lớn là 12%
         }
-        else if(random2<=0.9 && random2 >0.87){
+        if(random2<=0.9 && random2 >0.87){
             idxX=getRandomElementsFromArray(this.idx,3);// xác suất xuất hiện 3 ô lớn là 3%
         }
         for(let i=0;i< idxX.length;i++){
             this.data[idxX[i]]=this.count*2; // ô đặc biệt có điểm gấp đôi ô bình thường
         }
         var idxNull=getRandomElementsFromArray(this.idx,1)[0];
-        this.data[idxNull]=0; // chắc chắn có 1 ô null
+        this.data[idxNull]=0; // chắc chắn có ít nhất 1 ô null
         var idxS=getRandomElementsFromArray(this.idx,1)[0];
-        this.data[idxS]=this.count; // chắc chắn có 1 ô điểm bình thường
+        this.data[idxS]=this.count; // chắc chắn có ít nhất 1 ô điểm bình thường
         for(let i=0;i<this.idx.length;i++){ // xác suất xuất hiện ô null và ô điểm bình thường là 50 50
             var tmp = Math.random();
             if(tmp >=0.5){
